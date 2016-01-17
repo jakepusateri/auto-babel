7418f1ceb26acff3645699debef95121a9979a7d
Jake Pusateri <jakepusateri@gmail.com>
2016-01-16 16:14:42 -0800

    remove unneeded tolowercase

--- es-feature-list.js
+++ es-feature-list.js
@@ -1,4 +1,4 @@
 var opts = require('babel-features').options();
 console.log(JSON.stringify(opts.plugins.map(function (plugin) {
-    return 'babel-plugin-' + plugin.toLowerCase();
+    return 'babel-plugin-' + plugin;
 })));
