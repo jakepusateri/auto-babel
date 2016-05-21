e5a21fea00d91e5b94eb479d910ab9ae15c3061e
Jake Pusateri <jakepusateri@gmail.com>
2016-05-21 13:19:37 -0700

    update for node v6

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -2,7 +2,7 @@ source build/nvm/nvm.sh
 nvm deactivate
 mkdir -p build/results/
 #nvm ls-remote
-for VERSION in $(nvm ls-remote | perl -pe 's/\e\[?.*?[\@-~]//g' | grep "0.10\|0.12\|iojs\|v4\|v5")
+for VERSION in $(nvm ls-remote | perl -pe 's/\e\[?.*?[\@-~]//g' | grep "0.10\|0.12\|iojs\|v4\|v5\|v6")
 do
     nvm install $VERSION
     nvm run $VERSION es-feature-list.js | tail -n +2 > build/results/$VERSION.json
